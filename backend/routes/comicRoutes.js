c7227179f2418fedbfeaf5a2a4788ca347b3efca
SamKlep <shamdemann23@gmail.com>
2021-02-01 19:15:35 -0600

    Admin protected routes.

--- backend/routes/comicRoutes.js
+++ backend/routes/comicRoutes.js
@@ -7,9 +7,14 @@ import {
   deleteComic,
   updateComic,
 } from '../controllers/comicController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getComics).post(createComic)
+router.route('/').get(getComics).post(protect, admin, createComic)
 
-router.route('/:id').get(getComicById).delete(deleteComic).put(updateComic)
+router
+  .route('/:id')
+  .get(getComicById)
+  .delete(protect, admin, deleteComic)
+  .put(protect, admin, updateComic)
 
 export default router
