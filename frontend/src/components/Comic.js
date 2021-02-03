09f64524eaf357017517cd3f0af2bc5b3e37e1ca
SamKlep <shamdemann23@gmail.com>
2021-02-02 19:44:48 -0600

    Pills around issue #.

--- frontend/src/components/Comic.js
+++ frontend/src/components/Comic.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Badge } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-import { useSelector } from 'react-redux'
-
 const Comic = ({ comic }) => {
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
-
   return (
     <Card className='my-3 p-3 rounded text-center'>
       <Link to={`/comics/${comic._id}`}>
@@ -24,22 +19,13 @@ const Comic = ({ comic }) => {
         </Link>
 
         <Card.Text as='h3'>
-          <strong># {comic.issue}</strong>
+          <Badge variant='info' pill>
+            # {comic.issue}
+          </Badge>
         </Card.Text>
         <Card.Text as='h5'>{comic.year}</Card.Text>
         <Card.Text as='h5'>{comic.publisher}</Card.Text>
       </Card.Body>
-      {userInfo && userInfo.isAdmin && (
-        <>
-          <Button variant='outline-success' className='my-2 btn'>
-            Edit
-          </Button>
-
-          <Button variant='outline-danger' className='btn '>
-            x
-          </Button>
-        </>
-      )}
     </Card>
   )
 }
